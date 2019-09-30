42f61f547fb0c18eb09261d0e04bdb2a654a6144
Jhan <Jhan@10.20.40.43>
2019-09-30 11:56:46 -0500

    das

---
+++ src/lab02_felixmeza_jhanbolivar/Lab02_FelixMeza_JhanBolivar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab02_felixmeza_jhanbolivar;
+
+/**
+ *
+ * @author Jhan
+ */
+public class Lab02_FelixMeza_JhanBolivar {
+
+    
+    public static void main(String[] args) {
+        Interfaz inter = new Interfaz();
+        inter.setVisible(true);
+    }
+    
+}
